e91d0c7ffea85d7c85b9369fc647805f82b2f6a3
seokjun <eldehd113@gmail.com>
2025-04-07 20:48:40 +0900

    Feat: BoardService 인터페이스 추가

---
+++ src/main/java/com/example/newsfeed/boards/service/BoardServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.newsfeed.boards.service;
+
+import com.example.newsfeed.boards.dto.BoardResponseDto;
+import com.example.newsfeed.boards.entity.Board;
+import com.example.newsfeed.boards.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Override
+    public BoardResponseDto save(String title, String contents) {
+
+        User findUser = userRepository.findUserByUsername(username);
+
+        Board board = new Board(title, contents);
+        board.setUser(findUser);
+
+        Board board = boardRepository.save(board);
+        return new BoardResponseDto(board.getId(), board.getTitle(), board.getContents(),
+            findUser.getUsername());
+
+
+    }
+
+
+}
