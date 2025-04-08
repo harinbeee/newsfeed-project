42bf0e77e48d3d6205e0b14d275859e66ae286a8
hyun2y00 <pottq577@naver.com>
2025-04-08 19:38:12 +0900

    Rename: 유저명 필드 이름 수정 (name -> username)

--- src/main/java/com/example/newsfeed/users/entity/User.java
+++ src/main/java/com/example/newsfeed/users/entity/User.java
@@ -25,7 +25,7 @@ public class User extends BaseEntity {
     private String password;
 
     @Column(nullable = false)
-    private String name;
+    private String username;
 
     @Column(nullable = false)
     private String nickname;
