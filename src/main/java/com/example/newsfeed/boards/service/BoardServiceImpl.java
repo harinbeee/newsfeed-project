d300b703dc83c60bd1125893a181fa1849b44647
seokjun <eldehd113@gmail.com>
2025-04-08 20:09:05 +0900

    chore: : import

--- src/main/java/com/example/newsfeed/boards/service/BoardServiceImpl.java
+++ src/main/java/com/example/newsfeed/boards/service/BoardServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 @Service
