eb86f384618f9f54bcfffe02c93b8300b0f75f89
dnjs5024 <dnjs7469@naver.com>
2025-04-08 11:11:09 +0900

    Feat : User테이블 id,email 조회 구현 UserController > UserService 의존성 주입

--- src/main/java/com/example/newsfeed/users/serivce/UserServiceImpl.java
+++ src/main/java/com/example/newsfeed/users/serivce/UserServiceImpl.java
@@ -3,6 +3,6 @@ package com.example.newsfeed.users.serivce;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 
 }
