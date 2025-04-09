3b38a4b43319d384ff62a6b91f79d2df1c69a9cf
soonch67890 <soonch67890@gmail.com>
2025-04-09 10:12:24 +0900

    Rename: LoginServiceImpl 클래스 안 findEmail -> findUser

--- src/main/java/com/example/newsfeed/users/dto/UserSaveResponseDto.java
+++ src/main/java/com/example/newsfeed/users/dto/UserSaveResponseDto.java
@@ -9,50 +9,30 @@ import lombok.Getter;
 @AllArgsConstructor
 public class UserSaveResponseDto {
 
-  private Long id;
+    private Long id;
 
-  private String email;
+    private String email;
 
-  private String password;
+    private String password;
 
-<<<<<<< HEAD
-  private String name;
-=======
     private String username;
->>>>>>> 2d97b16dacc3024bace2a0714f79fac446a7c7ad
 
-  private String nickname;
+    private String nickname;
 
-  private String phone;
+    private String phone;
 
-  private String profilePicture;
+    private String profilePicture;
 
-  private String description;
+    private String description;
 
-  private boolean isDeleted;
+    private boolean isDeleted;
 
-  private LocalDateTime createdAt;
+    private LocalDateTime createdAt;
 
-  private LocalDateTime updatedAt;
+    private LocalDateTime updatedAt;
 
-  public static UserSaveResponseDto toDto(User user) { // user > UserSaveResponseDto 변환
+    public static UserSaveResponseDto toDto(User user) { // user > UserSaveResponseDto 변환
 
-<<<<<<< HEAD
-    return new UserSaveResponseDto(
-        user.getId(),
-        user.getEmail(),
-        user.getPassword(),
-        user.getName(),
-        user.getNickname(),
-        user.getPhone(),
-        user.getProfilePicture(),
-        user.getDescription(),
-        user.isDeleted(),
-        user.getCreatedAt(),
-        user.getUpdatedAt()
-    );
-  }
-=======
         return new UserSaveResponseDto(
             user.getId(),
             user.getEmail(),
@@ -67,5 +47,4 @@ public class UserSaveResponseDto {
             user.getUpdatedAt()
         );
     }
->>>>>>> 2d97b16dacc3024bace2a0714f79fac446a7c7ad
 }
