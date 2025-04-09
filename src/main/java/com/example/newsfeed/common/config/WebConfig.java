88ab6caead7de76a3aa027b3b2c4d460115f2d02
seokjun <eldehd113@gmail.com>
2025-04-09 15:20:03 +0900

    Rename: í config 패키지 이동

--- src/main/java/com/example/newsfeed/common/config/WebConfig.java
+++ src/main/java/com/example/newsfeed/common/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.newsfeed.login.config;
+package com.example.newsfeed.common.config;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
