b4debf94d7211ee18039576c1dc5ace01cb324f3
hyun2y00 <pottq577@naver.com>
2025-04-09 19:27:12 +0900

    Refactor: login 도메인 -> auth 도메인으로 이름 변경 및 컨트롤러, 서비스 통합

--- src/main/java/com/example/newsfeed/auth/controller/AuthController.java
+++ src/main/java/com/example/newsfeed/auth/controller/AuthController.java
@@ -1,8 +1,9 @@
-package com.example.newsfeed.login.controller;
+package com.example.newsfeed.auth.controller;
 
 
-import com.example.newsfeed.login.dto.LoginRequestDto;
-import com.example.newsfeed.login.service.LoginService;
+import com.example.newsfeed.auth.dto.LoginRequestDto;
+import com.example.newsfeed.auth.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
@@ -15,11 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping
 @RequiredArgsConstructor
-public class LoginController {
+public class AuthController {
 
-    private final LoginService loginService;
+    private final AuthService authService;
 
     /**
      * @param requestDto 이메일과 비밀번호 요청
@@ -27,10 +28,23 @@ public class LoginController {
      * @param response   쿠키에 세션id 저장
      * @return 응답코드 200 성공, 400 접근금지, 400 비밀번호 미일치
      */
-    @PostMapping
+    @PostMapping("/login")
     public ResponseEntity<Void> login(@RequestBody @Valid LoginRequestDto requestDto,
         HttpSession session, HttpServletResponse response) {
-        loginService.login(requestDto.getEmail(), requestDto.getPassword(), session, response);
+        authService.login(requestDto.getEmail(), requestDto.getPassword(), session, response);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * @param request  세션에 저장된 userid 요청
+     * @param response 쿠키를 만료
+     * @return 응답정보 200 성공, 401 미로그인
+     */
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response) {
+
+        authService.logout(request, response);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
--- src/main/java/com/example/newsfeed/auth/dto/LoginRequestDto.java
+++ src/main/java/com/example/newsfeed/auth/dto/LoginRequestDto.java
@@ -1,4 +1,4 @@
-package com.example.newsfeed.login.dto;
+package com.example.newsfeed.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
