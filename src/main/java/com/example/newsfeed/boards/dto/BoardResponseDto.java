cf7071ed47b3f040683f5f4aab0bbccff5eb139b
harinbeee <luv_lyn@naver.com>
2025-04-09 20:04:09 +0900

    Fix : 작성일 수정일 추가

--- src/main/java/com/example/newsfeed/boards/dto/BoardResponseDto.java
+++ src/main/java/com/example/newsfeed/boards/dto/BoardResponseDto.java
@@ -1,29 +1,36 @@
 package com.example.newsfeed.boards.dto;
 
 import com.example.newsfeed.boards.entity.Board;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class BoardResponseDto {
 
-    private final Long id;
+    private Long id;
 
-    private final String nickname;
+    private String nickname;
 
-    private final String title;
+    private String title;
 
-    private final String contents;
+    private String contents;
 
-    public BoardResponseDto(Long id, String title, String contents, String nickname) {
-        this.id = id;
-        this.nickname = nickname;
-        this.title = title;
-        this.contents = contents;
-    }
+    private LocalDateTime cratedAt;
+
+    private LocalDateTime updatedAt;
+
 
     // 전체 조회
     public static BoardResponseDto toDto(Board board) {
-        return new BoardResponseDto(board.getId(), board.getTitle(),
-            board.getContents(), board.getUser().getNickname());
+        return new BoardResponseDto(
+            board.getId(),
+            board.getUser().getNickname(),
+            board.getTitle(),
+            board.getContents(),
+            board.getCreatedAt(),
+            board.getUpdatedAt()
+        );
     }
 }
