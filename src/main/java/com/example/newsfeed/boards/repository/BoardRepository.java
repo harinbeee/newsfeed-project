2ed337e11a4b3c7a35fd085a5b89de49b90789d9
hyun2y00 <pottq577@naver.com>
2025-04-09 15:20:31 +0900

    Feat: 전역 예외 처리를 사용하도록 수정

--- src/main/java/com/example/newsfeed/boards/repository/BoardRepository.java
+++ src/main/java/com/example/newsfeed/boards/repository/BoardRepository.java
@@ -3,13 +3,13 @@ package com.example.newsfeed.boards.repository;
 
 import com.example.newsfeed.boards.dto.BoardPageResponseDto;
 import com.example.newsfeed.boards.entity.Board;
+import com.example.newsfeed.common.exception.BusinessException;
+import com.example.newsfeed.common.exception.ExceptionCode;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
@@ -17,8 +17,8 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     Optional<Board> findById(Long boardId);
 
     default Board findByIdOrElseThrow(Long boardId) {
-        return findById(boardId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-            "Does not exist id +" + boardId));
+        return findById(boardId).orElseThrow(
+            () -> new BusinessException(ExceptionCode.BOARD_NOT_FOUND));
     }
 
     @Query("""
