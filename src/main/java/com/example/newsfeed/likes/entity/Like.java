c29e5eabe882ded1e83bc47b15283961a1fea494
hyun2y00 <pottq577@naver.com>
2025-04-10 14:49:56 +0900

    Refactor: likes 도메인 Like 엔티티 롬복 어노테이션 사용하도록 수정

--- src/main/java/com/example/newsfeed/likes/entity/Like.java
+++ src/main/java/com/example/newsfeed/likes/entity/Like.java
@@ -11,11 +11,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Table(name = "likes")
+@NoArgsConstructor
 public class Like {
 
     @Id
@@ -39,7 +41,4 @@ public class Like {
         this.board = board;
     }
 
-    public Like() {
-
-    }
 }
