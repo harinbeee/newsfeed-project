55568bc40476464c7f79e5c2f3ab7b3b75040ff4
hyun2y00 <pottq577@naver.com>
2025-04-10 14:48:59 +0900

    Refactor: likes 도메인 DTO 불변 객체로 선언

--- src/main/java/com/example/newsfeed/likes/dto/LikeFindRequestDto.java
+++ src/main/java/com/example/newsfeed/likes/dto/LikeFindRequestDto.java
@@ -2,14 +2,16 @@ package com.example.newsfeed.likes.dto;
 
 import jakarta.validation.constraints.Min;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class LikeFindRequestDto {
 
     @Min(0)
-    private Long boardId;
+    private final Long boardId;
 
     @Min(0)
-    private Long commentId;
+    private final Long commentId;
 
 }
