0a3ceb95c402d18e665cb8788b2d9f492f044238
hyun2y00 <pottq577@naver.com>
2025-04-10 12:27:18 +0900

    Refactor: 요청 DTO 전달하도록 수정 및 JavaDoc 추가

--- src/main/java/com/example/newsfeed/boards/service/CommentService.java
+++ src/main/java/com/example/newsfeed/boards/service/CommentService.java
@@ -1,13 +1,29 @@
 package com.example.newsfeed.boards.service;
 
+import com.example.newsfeed.boards.dto.CommentRequestDto;
 import com.example.newsfeed.boards.dto.CommentResponseDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface CommentService {
 
-    CommentResponseDto save(Long userId, Long boardId, String contents);
+    /**
+     * 댓글 저장 요청 서비스
+     *
+     * @param userId     유저 식별자
+     * @param boardId    게시글 식별자
+     * @param requestDto 댓글 내용이 담긴 {@link CommentRequestDto} 객체
+     * @return 댓글 정보가 담긴 {@link CommentResponseDto} 객체
+     */
+    CommentResponseDto save(Long userId, Long boardId, CommentRequestDto requestDto);
 
+    /**
+     * 댓글 단건 조회 요청 서비스
+     *
+     * @param boardId   게시글 식별자
+     * @param commentId 댓글 식별자
+     * @return 댓글 정보가 담긴 {@link CommentResponseDto} 객체
+     */
     CommentResponseDto findOne(Long boardId, Long commentId);
 
 }
