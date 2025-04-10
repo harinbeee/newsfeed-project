2b6543da41aa83ae1f96ecd7f9ed9a0522a786a0
seokjun <eldehd113@gmail.com>
2025-04-10 11:37:20 +0900

    Refactor: ãgetSession()명시적 생성

--- src/main/java/com/example/newsfeed/auth/controller/AuthController.java
+++ src/main/java/com/example/newsfeed/auth/controller/AuthController.java
@@ -27,6 +27,27 @@ public class AuthController {
     private final UserService userService;
 
     /**
+     * 유저 회원가입 메소드
+     *
+     * @param requestDto { “email”: String, “password”: String, “username”: String, “nickname”:
+     *                   String, “phone”: String, “profile_picture”: String, “description”: String,
+     *                   }
+     * @return UserSaveResponseDto, 응답코드 200 성공 401 에러
+     */
+    @PostMapping("/signup")
+    public ResponseEntity<UserSaveResponseDto> signUp(
+        @RequestBody @Valid UserSaveRequestDto requestDto
+    ) {
+
+        userService.findByEmail(requestDto.getEmail()); // 입력한 이메일 이미 있는지 체크 중복이면 Exception
+
+        UserSaveResponseDto responseDto = authService.save(requestDto); // 중복 없으면 가입
+
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+
+    }
+
+    /**
      * @param requestDto 이메일과 비밀번호 요청
      * @param session    세션에 userid 입력
      * @param response   쿠키에 세션id 저장
@@ -35,9 +56,11 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<Void> login(
         @RequestBody @Valid LoginRequestDto requestDto,
-        HttpSession session,
+        HttpServletRequest request,
         HttpServletResponse response
     ) {
+
+        HttpSession session = request.getSession();
 
         authService.login(requestDto, session, response);
 
@@ -60,25 +83,5 @@ public class AuthController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    /**
-     * 유저 회원가입 메소드
-     *
-     * @param requestDto { “email”: String, “password”: String, “username”: String, “nickname”:
-     *                   String, “phone”: String, “profile_picture”: String, “description”: String,
-     *                   }
-     * @return UserSaveResponseDto, 응답코드 200 성공 401 에러
-     */
-    @PostMapping("/signup")
-    public ResponseEntity<UserSaveResponseDto> signUp(
-        @RequestBody @Valid UserSaveRequestDto requestDto
-    ) {
-
-        userService.findByEmail(requestDto.getEmail()); // 입력한 이메일 이미 있는지 체크 중복이면 Exception
-
-        UserSaveResponseDto responseDto = authService.save(requestDto); // 중복 없으면 가입
-
-        return new ResponseEntity<>(responseDto, HttpStatus.OK);
-
-    }
 
 }
