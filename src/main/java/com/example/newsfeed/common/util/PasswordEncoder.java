baf52682732fbe2c7a08d4885964545ee243cd1d
hyun2y00 <pottq577@naver.com>
2025-04-10 11:37:03 +0900

    Rename: PasswordEncorder util 패키지로 이동

--- src/main/java/com/example/newsfeed/common/util/PasswordEncoder.java
+++ src/main/java/com/example/newsfeed/common/util/PasswordEncoder.java
@@ -1,4 +1,4 @@
-package com.example.newsfeed.common.encoder;
+package com.example.newsfeed.common.util;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Component;
