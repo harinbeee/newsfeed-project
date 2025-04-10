55747ba6d988e4ae100167a65688e966b13a4ffe
hyun2y00 <pottq577@naver.com>
2025-04-10 11:19:56 +0900

    Style: boards 도메인 코드 스타일 단일화 및 JavaDoc 추가
    게시물 삭제 시 첫 번째 인자 name -> userId로 수정
    요청 dto 자체를 매개변수로 넘기도록 수정

--- src/main/java/com/example/newsfeed/boards/service/BoardService.java
+++ src/main/java/com/example/newsfeed/boards/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.example.newsfeed.boards.service;
 
 import com.example.newsfeed.boards.dto.BoardPageResponseDto;
+import com.example.newsfeed.boards.dto.BoardRequestDto;
 import com.example.newsfeed.boards.dto.BoardResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -8,14 +9,50 @@ import org.springframework.stereotype.Service;
 @Service
 public interface BoardService {
 
-    BoardResponseDto save(Long userId, String title, String contents);
+    /**
+     * 게시글 생성 요청 서비스
+     *
+     * @param userId     게시글을 작성하는 유저 식별자
+     * @param requestDto 게시글 작성 요청 내용이 담겨있는 {@link BoardRequestDto} 객체
+     * @return 작성된 게시글 정보가 담겨있는 {@link BoardResponseDto} 객체
+     */
+    BoardResponseDto save(Long userId, BoardRequestDto requestDto);
 
+    /**
+     * 게시글 전체 조회 요청 서비스
+     *
+     * @param page          현재 페이지
+     * @param size          페이지당 게시글 개수
+     * @param isFriendBoard true = 팔로우한 게시글 우선순위, false = 기본 정렬
+     * @param userId        친구 식별자
+     * @return 조회된 게시글 정보가 담겨있는 {@link Page} 객체
+     */
     Page<BoardPageResponseDto> findAll(int page, int size, boolean isFriendBoard, Long userId);
 
+    /**
+     * 게시글 단건 조회 요청 서비스
+     *
+     * @param boardId 게시글 식별자
+     * @return 조회된 게시글 정보가 담겨있는 {@link BoardResponseDto} 객체
+     */
     BoardResponseDto findOne(Long boardId);
 
-    BoardResponseDto update(Long boardId, Long userId, String title, String contents);
+    /**
+     * 게시글 수정 요청 서비스
+     *
+     * @param boardId    게시글 식별자
+     * @param userId     유저 식별자
+     * @param requestDto 게시글 수정 요청 정보가 담겨있는 {@link BoardRequestDto} 객체
+     * @return 수정된 게시글 정보가 담겨있는 {@link BoardResponseDto} 객체
+     */
+    BoardResponseDto update(Long boardId, Long userId, BoardRequestDto requestDto);
 
-    void delete(String name, Long boardId);
+    /**
+     * 게시글 삭제 요청 서비스
+     *
+     * @param userId  유저 식별자
+     * @param boardId 게시글 식별자
+     */
+    void delete(Long userId, Long boardId);
 
 }
