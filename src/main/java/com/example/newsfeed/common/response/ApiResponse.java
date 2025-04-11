2aaeca75073154f2919b246a82cd8842f4c08087
hyun2y00 <pottq577@naver.com>
2025-04-11 20:37:30 +0900

    Refactor: ApiResponse 불필요한 상수 제거

--- src/main/java/com/example/newsfeed/common/response/ApiResponse.java
+++ src/main/java/com/example/newsfeed/common/response/ApiResponse.java
@@ -11,10 +11,6 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public class ApiResponse<T> {
 
-    private static final String SUCCESS_STATUS = "success";
-    private static final String FAIL_STATUS = "fail";
-    private static final String ERROR_STATUS = "error";
-
     private final HttpStatus httpStatus;
     private final boolean success;
     private final T data;
